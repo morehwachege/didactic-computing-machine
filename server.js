8f1e741ac4e1ab448cac92bb4539ed6f4db4f8e1
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-15 13:57:52 +0300

    add favorite food

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -6,6 +7,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
